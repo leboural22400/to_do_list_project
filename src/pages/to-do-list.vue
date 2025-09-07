4a792727984ffaa2915e39b99b1a8828940ba23c
TheOnlyChou <alexandre.chou-a-wean-alias-lee-hung-hing@efrei.net>
2025-09-06 20:24:58 -0400

    Setup project

--- src/pages/to-do-list.vue
+++ src/pages/to-do-list.vue
@@ -1,28 +1,28 @@
-<script setup lang="ts">
-import {ref} from 'vue'
+<script setup lang="js">
+import { ref } from 'vue';
 
 const toDo = ref('')
 const toDoList = ref([])
 const taskHider = ref(false)
 
- const addTask=()=>{
-    toDoList.value.push({
-      title: toDo.value,
-      status: false,
-      date: Date.now()
-    })
-   toDo.value = ''
- }
+const addTask = () => {
+  toDoList.value.push({
+    title: toDo.value,
+    status: false,
+    date: Date.now()
+  })
+  toDo.value = ''
+}
 
- const sortedList = () => {
+const sortedList = () => {
 
   const sortedList = toDoList.value.toSorted((a, b) =>
-      a.status > b.status ? 1 : -1)
-   if (taskHider.value) {
+    a.status > b.status ? 1 : -1)
+  if (taskHider.value) {
     return sortedList.filter(c => c.status === false)
-   }
-   return sortedList
- }
+  }
+  return sortedList
+}
 </script>
 
 <template>
@@ -37,15 +37,16 @@ const taskHider = ref(false)
 
         <span>
           <input type="checkbox" v-model="toDo.status" />
-          {{toDo.title}}
+          {{ toDo.title }}
         </span>
 
       </li>
       <li>
-        <form class="add-task"  @submit.prevent="addTask"> <!-- .prevent empêche de recharger la page sinn tout s'efface -->
-        <input v-model="toDo" type="text" placeholder="Nouvelle tâche" class="task-input"/>
-        <button type="submit" class="add-button">Ajouter</button>
-      </form>
+        <form class="add-task" @submit.prevent="addTask">
+          <!-- .prevent empêche de recharger la page sinn tout s'efface -->
+          <input v-model="toDo" type="text" placeholder="Nouvelle tâche" class="task-input" />
+          <button type="submit" class="add-button">Ajouter</button>
+        </form>
       </li>
     </ul>
 
